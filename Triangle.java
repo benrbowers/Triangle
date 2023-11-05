40f4ef66ab28564fbe611846a79e1e0073a35f0b
benrbowers <benjambowers@gmail.com>
2023-11-05 09:56:34 -0500

    Use provided GeometricObject

--- Triangle.java
+++ Triangle.java
@@ -4,12 +4,16 @@ public class Triangle extends GeometricObject {
 	private double side3;
 
 	public Triangle() {
+		super();
+
 		side1 = 1.0;
 		side2 = 1.0;
 		side3 = 1.0;
 	}
 
 	public Triangle(double side1, double side2, double side3) {
+		super();
+		
 		this.side1 = side1;
 		this.side2 = side2;
 		this.side3 = side3;
